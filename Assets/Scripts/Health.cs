dfe07d62226e3f67298709fba5568f72f68cf72e
juneau <juneaugawat@hotmail.com>
2021-02-11 21:45:08 -0500

    added Health placeholder

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Health : MonoBehaviour
+{
+    public abstract float GameObjectHealth();
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : Health
+{
+    public override float GameObjectHealth()
+    {
+        return 0; // dont know how the systems gonna work yet.
+    }
+}
